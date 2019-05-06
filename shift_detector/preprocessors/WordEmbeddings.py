311c31d73389b465949b1b529bc07c74aa1814c6
Jan Westphal <jan.westphal306@gmail.com>
2019-05-06 11:40:24 +0200

    WIP

---
+++ shift_detector/preprocessors/WordEmbeddings.py
@@ -0,0 +1,9 @@
+from gensim.models import FastText, Word2Vec
+
+class Word2VecEmbedding():
+    def test(self):
+        return 0
+
+class FastTextEmbedding():
+    def test(self):
+        return 1
