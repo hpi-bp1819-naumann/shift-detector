112e3bd3f32ccfdbb960b1637a3d3aed37667f0c
Leonard <l.v.merzljak@mailbox.org>
2019-05-10 12:59:31 +0200

    Added comments and minor changes

--- shift_detector/checks/frequent_item_rules/fpgrowth.py
+++ shift_detector/checks/frequent_item_rules/fpgrowth.py
@@ -1,38 +1,34 @@
-import csv
 from collections import namedtuple
+
 from shift_detector.checks.frequent_item_rules import pyfpgrowth_core
 
-relative_min_support = 0.02
-min_confidence = 0.1
 
-
-def calculate_frequent_rules(df1, df2):
-
-    transactions = ([], [])
+def calculate_frequent_rules(df1, df2, relative_min_support: float = 0.01, relative_min_confidence: float = 0.3):
+    """Use fp-growth algorithm to calculate and compare conditional probabilities."""
+    transactions = tuple([] for _ in range(2))
     columns = df1.columns
     column_to_index = {c: i for i, c in enumerate(columns)}
 
-    # TODO rewrite whole method for pandas-DataFrame instead of [0] and [1] etc
     for index, row in df1.iterrows():
         transactions[0].append([(c, row[c]) for c in columns])
 
     for index, row in df2.iterrows():
         transactions[1].append([(c, row[c]) for c in columns])
 
-    absolute_min_supports = (round(relative_min_support * len(transactions[0])),
-                             round(relative_min_support * len(transactions[1])))
+    absolute_min_supports = tuple(round(relative_min_support * len(transactions[i])) for i in range(2))
 
-    patterns = (pyfpgrowth_core.find_frequent_patterns(transactions[0], absolute_min_supports[0]),
-                pyfpgrowth_core.find_frequent_patterns(transactions[1], absolute_min_supports[1]))
+    patterns = tuple(
+        pyfpgrowth_core.find_frequent_patterns(transactions[i], absolute_min_supports[i]) for i in range(2))
 
-    rules = (pyfpgrowth_core.generate_association_rules(patterns[0], min_confidence, len(transactions[0])),
-             pyfpgrowth_core.generate_association_rules(patterns[1], min_confidence, len(transactions[1])))
+    rules = (pyfpgrowth_core.generate_association_rules(patterns[0], relative_min_confidence, len(transactions[0])),
+             pyfpgrowth_core.generate_association_rules(patterns[1], relative_min_confidence, len(transactions[1])))
 
     Rule = namedtuple('Rule', ['left_side', 'right_side', 'supports_of_left_side', 'delta_supports_of_left_side',
                                'supports', 'delta_supports', 'confidences', 'delta_confidences'])
 
     result = []
     intersection = rules[0].keys() & rules[1].keys()
+    # compare rules that exceed min support in both data sets
     for key in intersection:
         result.append(
             Rule(key.left_side, key.right_side, (rules[0][key].support_of_left_side,
@@ -44,7 +40,9 @@ def calculate_frequent_rules(df1, df2):
                  (rules[0][key].confidence - rules[1][key].confidence))
         )
 
-    def calculate_support(exclusives, grouping_attributes, rule, other_transactions):
+    def get_absolute_supports(exclusives, grouping_attributes, other_transactions):
+        """Calculate and return absolute support of rules of `exclusives` in `other_transactions`"""
+        rule = {}
         for left_side, right_side in exclusives:
             rule[left_side] = 0
             rule[tuple(sorted(left_side + right_side))] = 0
@@ -56,16 +54,16 @@ def calculate_frequent_rules(df1, df2):
                 possible_key = tuple(transaction[i] for i in indexes)
                 if possible_key in rule:
                     rule[possible_key] += 1
+        return rule
 
     first_exclusives = rules[0].keys() - rules[1].keys()
+    # compare rules exceeding min support only in the first data set
     if first_exclusives:
-        exclusives = first_exclusives
         grouping_attributes = set()
-        rule = {}
         other_transactions = transactions[1]
-        calculate_support(exclusives, grouping_attributes, rule, other_transactions)
+        rule = get_absolute_supports(first_exclusives, grouping_attributes, other_transactions)
 
-        for key in exclusives:
+        for key in first_exclusives:
             support = rule[tuple(sorted(key.left_side + key.right_side))] / len(other_transactions)
             support_of_left_side = rule[key.left_side] / len(other_transactions)
             if support_of_left_side:
@@ -83,14 +81,13 @@ def calculate_frequent_rules(df1, df2):
             )
 
     second_exclusives = rules[1].keys() - rules[0].keys()
+    # compare rules exceeding min support only in the second data set
     if second_exclusives:
-        exclusives = second_exclusives
         grouping_attributes = set()
-        rule = {}
         other_transactions = transactions[0]
-        calculate_support(exclusives, grouping_attributes, rule, other_transactions)
+        rule = get_absolute_supports(second_exclusives, grouping_attributes, other_transactions)
 
-        for key in exclusives:
+        for key in second_exclusives:
             support = rule[tuple(sorted(key.left_side + key.right_side))] / len(other_transactions)
             support_of_left_side = rule[key.left_side] / len(other_transactions)
             if support_of_left_side:
@@ -106,9 +103,4 @@ def calculate_frequent_rules(df1, df2):
                     (confidence, rules[1][key].confidence), (confidence - rules[1][key].confidence)
                 )
             )
-            
     return result
-
-
-if __name__ == '__main__':
-    result = calculate_frequent_rules()
