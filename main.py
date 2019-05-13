8255cd57a1edc9896c71856fec89e0745790468d
Jan Westphal <jan.westphal306@gmail.com>
2019-05-13 15:54:50 +0200

    Postprocessing (#10)
    * Implement basic result history
    
    * Integrate result history in detector architecture
    
    * Rename result to report and improve documentation
    
    * Improve definition of check

--- main.py
+++ main.py
@@ -10,21 +10,25 @@ from shift_detector.checks.TestCheck import TestCheck
 # python3 main.py --train ./train_ascii.csv --test ./audits_ascii.csv --sep  ";"
 if __name__ == "__main__":
     ap = argparse.ArgumentParser()
-    # ap.add_argument("-train", "--train", required=True, help="path of train dataset")
-    # ap.add_argument("-test", "--test", required=True, help="path of test dataset")
-    # ap.add_argument("-s", "--sep", required=True, help="separator for datasets")
+    ap.add_argument("-train", "--train", required=True, help="path of train dataset")
+    ap.add_argument("-test", "--test", required=True, help="path of test dataset")
+    ap.add_argument("-s", "--sep", required=True, help="separator for datasets")
     args = vars(ap.parse_args())
 
-    args = {'train': '/Users/pzimme/Desktop/Datasets/audits_leonard.csv', 'test':'/Users/pzimme/Desktop/Datasets/train_leonard.csv', 'sep': ','}
-
+    # args = {'train': '/Users/pzimme/Desktop/Datasets/audits_leonard.csv', 'test':'/Users/pzimme/Desktop/Datasets/train_leonard.csv', 'sep': ','}
     train_path = args["train"]
     audits_path = args["test"]
     separator = args["sep"]
     
-    Detector(train_path, audits_path, separator=separator) \
-        .add_check(Chi2Check()) \
-        .add_check(TestCheck()) \
-        .run()
-        
+    detector = Detector(train_path, audits_path, separator=separator)
+    detector.add_check(Chi2Check())
+    
+    detector.run()
+    detector.evaluate()
+    print()
+    
+    detector.checks_reports[0].reports.evaluate(significance=0.5)
+    detector.evaluate()
+
     # modules=[])
     # .add_check(KsChiCheck) \
