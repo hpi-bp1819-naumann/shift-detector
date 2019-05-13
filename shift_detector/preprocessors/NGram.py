2d5a02cc956c7e60ebd8bb7a6aba35bea27f4fd9
Philipp Schmidt <scholl.8.st@googlemail.com>
2019-05-13 14:33:13 +0200

    add NGram preprocessor

---
+++ shift_detector/preprocessors/NGram.py
@@ -0,0 +1,33 @@
+from enum import Enum
+import numpy as np
+from gensim.models import FastText, Word2Vec
+from numbers import Number
+from copy import copy
+
+
+class NGram:
+
+    def __init__(self, n=5):
+        self.n = n
+        if self.n < 1:
+            raise Exception('n has to be greater than 0')
+
+    def __eq__(self, other):
+        """Overrides the default implementation"""
+        return self.histogram_type == other.histogram_type
+
+    def process(self, train, test):
+
+        def generate_ngram(text, n):
+            ngram = {}
+            for i in range(len(text) - n):
+                ngram[text[i:i+n]] = 1 if text[i:i+n] not in ngram else ngram[text[i:i+n]] + 1
+            return ngram
+
+        train = train.dropna().str.lower()
+        train_processed = train.apply(lambda row: generate_ngram(row, self.n))
+
+        test = test.dropna().str.lower()
+        test_processed = test.apply(lambda row: generate_ngram(row, self.n))
+
+        return train_processed, test_processed
