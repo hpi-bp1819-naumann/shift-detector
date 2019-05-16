886077cc3802c8ed2d8ec3275dd0857c444d8ffd
Philipp Schmidt <scholl.8.st@googlemail.com>
2019-05-16 10:11:13 +0200

    join character and word ngram to one function

--- shift_detector/preprocessors/NGram.py
+++ shift_detector/preprocessors/NGram.py
@@ -1,21 +1,44 @@
 from shift_detector.preprocessors.Preprocessor import Preprocessor
 from abc import ABCMeta, abstractmethod
+from enum import Enum
 
 
-class NGram(Preprocessor, metaclass=ABCMeta):
+class NGramType(Enum):
+    word = "word"
+    character = "character"
 
-    def __init__(self, n):
+
+class NGram(Preprocessor):
+
+    def __init__(self, n: int, ngram_type : NGramType):
         self.n = n
+        self.ngram_type = ngram_type
         if self.n < 1:
             raise ValueError('n has to be greater than 0')
 
     def __eq__(self, other):
         """Overrides the default implementation"""
-        return self.n == other.n
+        return self.n == other.n and self.ngram_type == other.ngram_type
 
     def __hash__(self):
-        return hash((self.__class__, self.n))
+        return hash((self.__class__, self.n, self.ngram_type))
 
-    @abstractmethod
-    def process(self, first_df, second_df):
-        return first_df, second_df
+    def generate_ngram(self, text):
+        ngram = {}
+        for i in range(len(text) - self.n + 1):
+            ngram[tuple(text[i:i + self.n])] = 1 if tuple(text[i:i + self.n]) not in ngram \
+                else ngram[tuple(text[i:i + self.n])] + 1
+        return ngram
+
+    def process(self, train, test):
+        if self.ngram_type == NGramType.word:
+            train = train.dropna().str.lower().str.split()
+            test = test.dropna().str.lower().str.split()
+        else:
+            train = train.dropna().str.lower()
+            test = test.dropna().str.lower()
+
+        train_processed = train.apply(lambda row: self.generate_ngram(list(row)))
+        test_processed = test.apply(lambda row: self.generate_ngram(list(row)))
+
+        return train_processed, test_processed
--- tests/preprocessors/test_NGram.py
+++ tests/preprocessors/test_NGram.py
@@ -1,32 +1,38 @@
 import unittest
-from unittest.mock import patch
-from shift_detector.preprocessors.NGram import NGram
+from shift_detector.preprocessors.NGram import NGram, NGramType
 import pandas as pd
 
 
 class TestNGram(unittest.TestCase):
 
-    @patch.multiple(NGram, __abstractmethods__=set())
     def setUp(self):
-        self.ng1 = NGram(5)
-        self.ng2 = NGram(6)
-        self.ng3 = NGram(6)
+        self.wordng1 = NGram(1, NGramType.word)
+        self.wordng2 = NGram(1, NGramType.word)
+        self.wordng3 = NGram(2, NGramType.word)
+        self.charng1 = NGram(1, NGramType.character)
+        self.charng2 = NGram(5, NGramType.character)
         self.ser1 = pd.Series(['Hello World', 'Foo Bar Baz'])
         self.ser2 = pd.Series(['TestText1', 'TestText2'])
+        self.ser3 = pd.Series(['abababa'])
 
     def test_eq(self):
-        self.assertTrue(self.ng2 == self.ng3)
-        self.assertFalse(self.ng1 == self.ng2)
+        self.assertTrue(self.wordng1 == self.wordng2)
+        self.assertFalse(self.wordng1 == self.wordng3)
+        self.assertFalse(self.wordng1 == self.charng1)
 
-    @patch.multiple(NGram, __abstractmethods__=set())
     def test_exception_on_small_n(self):
-        self.assertRaises(ValueError, lambda: NGram(0))
-        self.assertRaises(ValueError, lambda: NGram(-1))
+        self.assertRaises(ValueError, lambda: NGram(0, NGramType.word))
+        self.assertRaises(ValueError, lambda: NGram(-1, NGramType.character))
 
     def test_hash(self):
-        self.assertEqual(hash(self.ng2), hash(self.ng3))
+        self.assertEqual(hash(self.wordng1), hash(self.wordng2))
+
+    def test_generate_ngram(self):
+        self.assertDictEqual(self.charng1.generate_ngram(('t', 'e', 's', 't')), {('t',): 2, ('e',): 1, ('s',): 1})
 
     def test_process(self):
-        res1, res2 = self.ng1.process(self.ser1, self.ser2)
-        self.assertListEqual(list(res1), list(self.ser1))
-        self.assertListEqual(list(res2), list(self.ser2))
+        res1, res2 = self.wordng1.process(self.ser1, self.ser2)
+        self.assertListEqual(list(res2), [{('testtext1',): 1}, {('testtext2',): 1}])
+        res3, res4 = self.charng2.process(self.ser1, self.ser3)
+        self.assertListEqual(list(res4), [{('a', 'b', 'a', 'b', 'a'): 2,
+                                           ('b', 'a', 'b', 'a', 'b'): 1}])
