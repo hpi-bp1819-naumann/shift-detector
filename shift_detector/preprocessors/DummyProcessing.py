dbf7e934dd93456a5f15fba96ce1bba766f1586f
Jan Westphal <jan.westphal306@gmail.com>
2019-05-17 14:42:10 +0200

    Implement dummy check and preprocessing

---
+++ shift_detector/preprocessors/DummyProcessing.py
@@ -0,0 +1,24 @@
+from pandas import DataFrame
+
+from shift_detector.preprocessors.Preprocessor import Preprocessor
+from shift_detector.preprocessors.Store import Store
+
+
+class DummyPreprocessing(Preprocessor):
+
+    def __init__(self, dummy_parameter=2):
+        self.parameter = dummy_parameter
+
+    def __eq__(self, other):
+        if not isinstance(other, self.__class__):
+            return False
+        return self.parameter == other.parameter
+
+    def __hash__(self):
+        return hash(tuple([self.__class__, self.parameter]))
+
+    def process(self, store: Store) -> (DataFrame, DataFrame):
+        df1_numerical, df2_numerical = store.numerical()
+        df1_processed = df1_numerical + self.parameter
+        df2_processed = df2_numerical + self.parameter
+        return df1_processed, df2_processed
