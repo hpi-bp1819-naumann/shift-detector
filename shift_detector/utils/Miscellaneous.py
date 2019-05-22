087eca5820431917264b928cb3419e4f8aa0164f
Justus Zeinert <zeinertj@amazon.com>
2019-05-22 16:37:17 +0200

    remove unrelated files

--- shift_detector/utils/Miscellaneous.py
+++
@@ -1,8 +0,0 @@
-def print_progress_bar(iteration, total, length):
-    percent = ("{0:.2f}").format(100 * (iteration / float(total)))
-    filledLength = int(length * iteration // total)
-    bar = '█' * filledLength + '-' * (length - filledLength)
-    print('\rProgress: |%s| %s%% Complete' % (bar, percent), end='\r')
-    # Print New Line on Complete
-    if iteration == total:
-        print('\n')
