6d5b8f25e57d52822e66bdcff251bdaecce0e5a0
Paul Zimmermann <p.zimmermann55@gmail.com>
2019-05-24 10:25:47 +0200

    refactoring all github conversations resolved

--- tests/checks/test_simple_check.py
+++ tests/checks/test_simple_check.py
@@ -22,16 +22,12 @@ class TestSimpleCheck(unittest.TestCase):
         self.store_numerical = Store(numerical_df_1, numerical_df_2)
         self.store_categorical = Store(categorical_df_1, categorical_df_2)
 
-    def test_basic_functions(self):
-        self.assertEqual(self.check.name(), 'SimpleCheck')
-        self.assertEqual(self.check.report_class(), SimpleCheckReport)
-
     def test_run_with_empty_dataframes(self):
         empty_store = Store(pd.DataFrame([]), pd.DataFrame([]))
-        self.assertEqual({'categorical_comparison': {}, 'numerical_comparison': {}}, self.check.run(empty_store))
+        self.assertEqual({'categorical_comparison': {}, 'numerical_comparison': {}}, self.check.run(empty_store).data)
 
     def test_minmax_metrics(self):
-        comparison_numeric = self.check.run(self.store_numerical)['numerical_comparison']
+        comparison_numeric = self.check.run(self.store_numerical).data['numerical_comparison']
 
         self.assertEqual(comparison_numeric['col_1']['min']['df1'], 1)
         self.assertEqual(comparison_numeric['col_2']['min']['df1'], 2)
@@ -52,13 +48,13 @@ class TestSimpleCheck(unittest.TestCase):
         numerical_df_2 = pd.DataFrame([[1], [3]], columns=['col_1'])
         new_numerical_store = Store(numerical_df_1, numerical_df_2)
 
-        comparison_numeric = self.check.run(new_numerical_store)['numerical_comparison']
+        comparison_numeric = self.check.run(new_numerical_store).data['numerical_comparison']
         self.assertEqual(comparison_numeric['col_1']['quartile_1']['df1'], 1)
         self.assertEqual(comparison_numeric['col_1']['quartile_3']['df1'], 3)
         self.assertEqual(comparison_numeric['col_1']['median']['df1'], 2)
 
     def test_mean_std(self):
-        comparison_numeric = self.check.run(self.store_numerical)['numerical_comparison']
+        comparison_numeric = self.check.run(self.store_numerical).data['numerical_comparison']
 
         self.assertEqual(comparison_numeric['col_1']['mean']['df1'], 2.5)
         self.assertEqual(comparison_numeric['col_2']['mean']['df1'], 2)
@@ -75,7 +71,7 @@ class TestSimpleCheck(unittest.TestCase):
         self.assertEqual(round(comparison_numeric['col_3']['std']['df2'], 3), 0)
 
     def test_uniqueness(self):
-        comparison_numeric = self.check.run(self.store_numerical)['numerical_comparison']
+        comparison_numeric = self.check.run(self.store_numerical).data['numerical_comparison']
 
         self.assertEqual(comparison_numeric['col_1']['uniqueness']['df1'], 1.0)
         self.assertEqual(comparison_numeric['col_2']['uniqueness']['df1'], 0)
@@ -85,7 +81,7 @@ class TestSimpleCheck(unittest.TestCase):
         self.assertEqual(comparison_numeric['col_3']['uniqueness']['df2'], 0)
 
     def test_distinctness(self):
-        comparison_numeric = self.check.run(self.store_numerical)['numerical_comparison']
+        comparison_numeric = self.check.run(self.store_numerical).data['numerical_comparison']
 
         self.assertEqual(comparison_numeric['col_1']['distinctness']['df1'], 1.0)
         self.assertEqual(comparison_numeric['col_2']['distinctness']['df1'], 0.5)
@@ -95,7 +91,7 @@ class TestSimpleCheck(unittest.TestCase):
         self.assertEqual(comparison_numeric['col_3']['distinctness']['df2'], 0.5)
 
     def test_completeness(self):
-        comparison_numeric = self.check.run(self.store_numerical)['numerical_comparison']
+        comparison_numeric = self.check.run(self.store_numerical).data['numerical_comparison']
 
         self.assertEqual(comparison_numeric['col_1']['completeness']['df1'], 1.0)
         self.assertEqual(comparison_numeric['col_2']['completeness']['df1'], 1.0)
@@ -105,7 +101,7 @@ class TestSimpleCheck(unittest.TestCase):
         self.assertEqual(comparison_numeric['col_3']['completeness']['df2'], 1.0)
 
     def test_categorical_values(self):
-        comparison_categorical = self.check.run(self.store_categorical)['categorical_comparison']
+        comparison_categorical = self.check.run(self.store_categorical).data['categorical_comparison']
         pp.pprint(comparison_categorical)
 
         self.assertEqual(comparison_categorical[0]['blue']['df1'], 1/3)
