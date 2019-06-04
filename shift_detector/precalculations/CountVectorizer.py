6f9812e2915832b8a9ef7cff98b215f104220990
Malte Barth <malte.barth@student.hpi.de>
2019-06-04 14:49:51 +0200

    remove external dataset dependency

--- shift_detector/precalculations/CountVectorizer.py
+++ shift_detector/precalculations/CountVectorizer.py
@@ -5,10 +5,9 @@ from sklearn.feature_extraction.text import CountVectorizer as CountVectorizer_s
 from shift_detector.Utils import ColumnType
 
 
-
 class CountVectorizer(Precalculation):
 
-    def __init__(self, stop_words='english', max_features=1000):
+    def __init__(self, stop_words='english', max_features=None):
         self.stopwords = None
         self.max_features = None
         if isinstance(stop_words, str):
@@ -22,22 +21,21 @@ class CountVectorizer(Precalculation):
                         self.stopwords = self.stopwords.union(set(stopwords.words(lang)))
         else:
             raise Exception('Please enter the language for your stopwords as a string or list of strings')
-        if isinstance(max_features, int):
-            if max_features > 0:
-                self.max_features = max_features
+        if max_features:
+            if isinstance(max_features, int):
+                if max_features > 0:
+                    self.max_features = max_features
+                else:
+                    raise ValueError('Max_features has to be at least 1')
             else:
-                raise ValueError('Max_features has to be at least 1')
-        else:
-            raise Exception('Max_features has to be an int')
+                raise Exception('Max_features has to be an int')
         self.vectorizer = CountVectorizer_sklearn(stop_words=self.stopwords, max_features=self.max_features)
 
-
-    
     def __eq__(self, other):
         """Overrides the default implementation"""
         if isinstance(other, self.__class__) and self.stop_words == other.stop_words \
                 and self.max_features == other.max_features:
-                    return True
+            return True
         return False
 
     def __hash__(self):
@@ -46,10 +44,10 @@ class CountVectorizer(Precalculation):
         return hash(tuple([self.__class__, self.stop_words, self.max_features]))
 
     def process(self, store):
-        train_texts, test_texts = store[ColumnType.text]
+        train_texts, test_texts = store[ColumnType.text]  # I need Series here
         merged_texts = pd.concat([train_texts, test_texts], ignore_index=True)
         self.vectorizer = self.vectorizer.fit(merged_texts)
         vectorized_merged = self.vectorizer.transform(merged_texts)
-        vectorized_train = CountVectorizer_sklearn().transform(train_texts)
-        vectorized_test = CountVectorizer_sklearn().transform(test_texts)
+        vectorized_train = self.vectorizer.transform(train_texts)
+        vectorized_test = self.vectorizer.transform(test_texts)
         return vectorized_merged, vectorized_train, vectorized_test
