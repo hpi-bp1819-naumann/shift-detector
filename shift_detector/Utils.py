c8fee603e83cc03216490d5efce0f412fcbd0bd6
Jan Westphal <jan.westphal306@gmail.com>
2019-06-05 14:57:25 +0200

    Change logging in column type detection

--- shift_detector/Utils.py
+++ shift_detector/Utils.py
@@ -80,12 +80,12 @@ def split_dataframes(df1: pd.DataFrame, df2: pd.DataFrame, columns: List) -> Dic
     }
     """
     numerical_columns = [c for c in columns if is_numeric_dtype(df1[c])
-                       and is_numeric_dtype(df2[c])]
-    logger.info("Assuming numerical columns: {}".format(", ".join(column_names(numerical_columns))))
+                         and is_numeric_dtype(df2[c])]
+    logger.info("Detected numerical columns: {}".format(", ".join(column_names(numerical_columns))))
     remaining_columns = list(set(columns) - set(numerical_columns))
 
     categorical_columns = [c for c in remaining_columns if is_categorical(df1[c]) and is_categorical(df2[c])]
-    logger.info("Assuming categorical columns: {}".format(", ".join(column_names(categorical_columns))))
+    logger.info("Detected categorical columns: {}".format(", ".join(column_names(categorical_columns))))
 
     numeric_categorical_columns = [c for c in numerical_columns if is_categorical(df1[c]) and is_categorical(df2[c])]
 
@@ -93,7 +93,7 @@ def split_dataframes(df1: pd.DataFrame, df2: pd.DataFrame, columns: List) -> Dic
     all_categorical_columns.extend(numeric_categorical_columns)
 
     text_columns = list(set(columns) - set(numerical_columns) - set(categorical_columns))
-    logger.info("Assuming text columns: {}".format(", ".join(column_names(text_columns))))
+    logger.info("Detected text columns: {}".format(", ".join(column_names(text_columns))))
 
     return {
         ColumnType.numerical: (df1[numerical_columns], df2[numerical_columns]),
