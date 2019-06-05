418bbf57f7365af718833ef7496f1b395dd067a0
Leonard <l.v.merzljak@mailbox.org>
2019-06-05 10:11:27 +0200

    Add further parameters and imrpove code

--- shift_detector/precalculations/ConditionalProbabilitiesPrecalculation.py
+++ shift_detector/precalculations/ConditionalProbabilitiesPrecalculation.py
@@ -1,27 +1,32 @@
 from shift_detector.Utils import ColumnType
-from shift_detector.precalculations.frequent_item_rules import fpgrowth
-from shift_detector.precalculations.frequent_item_rules import rule_compression
 from shift_detector.precalculations.Precalculation import Precalculation
+from shift_detector.precalculations.conditional_probabilities import fpgrowth
+from shift_detector.precalculations.conditional_probabilities import rule_compression
 
 
-class FrequentItemsetPrecalculation(Precalculation):
+class ConditionalProbabilitiesPrecalculation(Precalculation):
 
-    def __init__(self, min_support=0.01, min_confidence=0.15):
+    def __init__(self, min_support, min_confidence, min_delta_supports, min_delta_confidences):
         self.min_support = min_support
         self.min_confidence = min_confidence
+        self.min_delta_supports = min_delta_supports
+        self.min_delta_confidences = min_delta_confidences
 
     def process(self, store):
         df1, df2 = store[ColumnType.categorical]
 
-        item_rules = fpgrowth.calculate_frequent_rules(df1, df2, self.min_support, self.min_confidence)
-        compressed_rules = rule_compression.compress_rules(item_rules)
+        uncompressed_rules = fpgrowth.calculate_frequent_rules(df1, df2, self.min_support, self.min_confidence,
+                                                               self.min_delta_supports, self.min_delta_confidences)
+        compressed_rules = rule_compression.compress_rules(uncompressed_rules)
 
-        return {'compressed_rules': compressed_rules, 'examined_columns': set(df1.columns)}
+        return compressed_rules, set(df1.columns)
 
     def __eq__(self, other):
         if not isinstance(other, self.__class__):
             return False
-        return self.min_support == other.min_support and self.min_confidence == other.min_confidence
+        return (self.min_support, self.min_confidence, self.min_delta_supports, self.min_delta_confidences) == (
+            other.min_support, other.min_confidence, other.min_delta_supports, other.min_delta_confidences)
 
     def __hash__(self):
-        return hash((self.__class__, self.min_support, self.min_confidence))
+        return hash((self.__class__, self.min_support, self.min_confidence, self.min_delta_supports,
+                     self.min_delta_confidences))
