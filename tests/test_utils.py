1912f10b92b8cc883612a68283ec4aaee255f57e
Justus Zeinert <zeinertj@amazon.com>
2019-06-06 09:09:41 +0200

    add test for UCBList block method

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 from shift_detector.utils.ColumnManagement import is_categorical, split_dataframes, ColumnType
 from shift_detector.utils.DataIO import shared_column_names
+from shift_detector.utils.UCBlist import block
 
 
 class TestUtils(unittest.TestCase):
@@ -57,3 +58,7 @@ class TestUtils(unittest.TestCase):
         self.assertListEqual(list(categorical_columns), list(['brand']))
         self.assertListEqual(list(all_categorical_columns), list(['brand', 'payment']))
         self.assertListEqual(list(text_columns), list(['description']))
+
+    def test_ucblist(self):
+        self.assertEqual('Basic Latin', block('L'))
+        self.assertEqual('CJK Unified Ideographs', block('中'))
