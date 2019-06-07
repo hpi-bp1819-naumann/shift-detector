966f7e4c593e6a5182fc4fb54786e85b297d4e6b
Leonard <l.v.merzljak@mailbox.org>
2019-06-07 11:42:18 +0200

    Include binned numerical columns

--- shift_detector/precalculations/ConditionalProbabilitiesPrecalculation.py
+++ shift_detector/precalculations/ConditionalProbabilitiesPrecalculation.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+from shift_detector.precalculations.BinningPrecalculation import BinningPrecalculation
 from shift_detector.precalculations.Precalculation import Precalculation
 from shift_detector.precalculations.conditional_probabilities import fpgrowth
 from shift_detector.utils.ColumnManagement import ColumnType
@@ -12,7 +15,12 @@ class ConditionalProbabilitiesPrecalculation(Precalculation):
         self.min_delta_confidences = min_delta_confidences
 
     def process(self, store):
-        df1, df2 = store[ColumnType.categorical]
+        df1_cat, df2_cat = store[ColumnType.categorical]
+        df1_num, df2_num = store[BinningPrecalculation()]
+
+        df1 = pd.concat([df1_cat, df1_num], axis=1)
+        df2 = pd.concat([df2_cat, df2_num], axis=1)
+
         rules = fpgrowth.calculate_frequent_rules(df1, df2, self.min_support, self.min_confidence,
                                                   self.min_delta_supports, self.min_delta_confidences)
         return rules, list(df1.columns)
