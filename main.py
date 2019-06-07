6f2abf2bf97ce0714cc7e82af02e86a7a079545c
Leonard <l.v.merzljak@mailbox.org>
2019-06-07 11:47:33 +0200

    Overwrite parameters

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ if __name__ == "__main__":
             'test': '/Users/merzljl/Desktop/audits.csv', 'sep': ','}
 
     detector = Detector(args['train'], args['test'], delimiter=args['sep'])
-    detector.run(ConditionalProbabilitiesCheck())
+    detector.run(ConditionalProbabilitiesCheck(min_support=0.01, min_confidence=0.01, rule_limit=100,
+                                               min_delta_supports=0, min_delta_confidences=0))
     detector.evaluate()
