1f4ad91b00d2358ecc74f0dfc6e7a401d0eb44ef
Leonard <l.v.merzljak@mailbox.org>
2019-06-07 10:35:36 +0200

    Improve documentation and safety

--- shift_detector/checks/ConditionalProbabilitiesCheck.py
+++ shift_detector/checks/ConditionalProbabilitiesCheck.py
@@ -11,33 +11,32 @@ class ConditionalProbabilitiesCheck(Check):
 
     :param min_support: a float between 0 and 1. This parameter mainly impacts
         the runtime of the check. The lower ``min_support`` the more resources are
-        required during computation both in terms of memory and CPU. The default
-        value is 0.01, which is high enough to get a reasonably good performance
-        and still low enough to not prematurely exclude significant association rules.
-        This parameter allows you to adjust the granularity of the comparison of the
-        two data sets.
-    :param min_confidence: a float between 0 and 1. This parameter impacts the amount
-        of generated association rules. The higher ``min_confidence`` the more rules
-        are generated.
-    :param rule_limit: this parameter expects an int. At most
-        ``rule_limit`` rules are printed as a result of executing this check.
-        The rules are sorted according to their significance.
-    :param min_delta_supports: a float between 0 and 1. Association rules whose support
+        required both in terms of memory and CPU.
+    :param min_confidence: a float between 0 and 1. This parameter impacts the size
+        of the result. The higher ``min_confidence`` the more rules are generated.
+    :param rule_limit: an int greater than or equal to 0. At most ``rule_limit``
+        rules are printed as a result of executing this check. The rules are sorted
+        according to their significance.
+    :param min_delta_supports: a float between 0 and 1. Rules whose support
         values exhibit an absolute difference of less than ``min_delta_supports`` are pruned.
-    :param min_delta_confidences: a float between 0 and 1. Association rules whose confidence
+    :param min_delta_confidences: a float between 0 and 1. Rules whose confidence
         values exhibit an absolute difference of less than ``min_delta_confidences`` are pruned.
     """
 
     def __init__(self, min_support=0.01, min_confidence=0.15, rule_limit=5, min_delta_supports=0.05,
                  min_delta_confidences=0.05):
-        self.min_support = min_support
-        self.min_confidence = min_confidence
-        self.rule_limit = rule_limit
-        self.min_delta_supports = min_delta_supports
-        self.min_delta_confidences = min_delta_confidences
+        assert 0 <= min_support <= 1, 'min_support expects a float between 0 and 1'
+        assert 0 <= min_confidence <= 1, 'min_confidence expects a float between 0 and 1'
+        assert 0 <= rule_limit, 'rule_limit expects an int greater than or equal to 0'
+        assert 0 <= min_delta_supports <= 1, 'min_delta_supports expects a float between 0 and 1'
+        assert 0 <= min_delta_confidences <= 1, 'min_delta_confidences expects a float between 0 and 1'
+        self.min_support = float(min_support)
+        self.min_confidence = float(min_confidence)
+        self.rule_limit = int(rule_limit)
+        self.min_delta_supports = float(min_delta_supports)
+        self.min_delta_confidences = float(min_delta_confidences)
 
     def run(self, store):
-        """Compute conditional probabilities and return a report."""
         rules, examined_columns = store[
             ConditionalProbabilitiesPrecalculation(self.min_support, self.min_confidence, self.min_delta_supports,
                                                    self.min_delta_confidences)]
