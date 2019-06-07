bcfece5625de094c3908579a29c722cc26df7c4e
Jan Westphal <jan.westphal306@gmail.com>
2019-06-07 15:28:07 +0200

    Include the number of epochs in the equality of distinction precalcul… (#52)
    * Include the number of epochs in the equality of distinction precalculation

--- tests/precalculations/test_DistinctionPrecalculation.py
+++ tests/precalculations/test_DistinctionPrecalculation.py
@@ -14,10 +14,22 @@ class TestCreateDetector(TestCase):
         self.df1 = DataFrame.from_dict(sales1)
         self.df2 = DataFrame.from_dict(sales2)
         self.store = Store(self.df1, self.df2)
-        self.precalculation = DistinctionPrecalculation(['shift', 'no_shift'])
+        self.precalculation = DistinctionPrecalculation(['shift', 'no_shift'], num_epochs=10)
+
+    def test_init(self):
+        with self.subTest("Test wrong columns"):
+            self.assertRaises(TypeError, lambda: DistinctionPrecalculation(['shift', 0]))
+
+        with self.subTest("Test wrong num epochs"):
+            self.assertRaises(Exception, lambda: DistinctionPrecalculation(columns=['shift'], num_epochs='wrong'))
+            self.assertRaises(Exception, lambda: DistinctionPrecalculation(columns=['shift'], num_epochs=0))
 
     def test_process(self):
-        calculation = self.precalculation.process(self.store)
+        examined_column, calculation = self.precalculation.process(self.store)
+
+        with self.subTest("Test examined column"):
+            self.assertCountEqual(examined_column, ['shift', 'no_shift'])
+
         with self.subTest("Test prediction"):
             y_true = calculation['y_true']
             y_pred = calculation['y_pred']
@@ -34,13 +46,21 @@ class TestCreateDetector(TestCase):
             no_shift_permuted_accuracy = calculation['permuted_accuracies']['no_shift']
             self.assertEqual(no_shift_permuted_accuracy, 1.)
 
+        with self.subTest("Test wrong columns"):
+            prec_wrong_columns = DistinctionPrecalculation(['wrong_column'], num_epochs=10)
+            self.assertRaises(Exception, lambda: prec_wrong_columns.process(self.store))
+
     def test_equal(self):
         with self.subTest("Test equality"):
             other_precalculation = DistinctionPrecalculation(['no_shift', 'shift'])
             self.assertEqual(self.precalculation, other_precalculation)
 
-        with self.subTest("Test inequality"):
-            other_precalculation = DistinctionPrecalculation(['no_shift'])
+        with self.subTest("Test inequality with other columns"):
+            other_precalculation = DistinctionPrecalculation(['no_shift'], num_epochs=10)
+            self.assertNotEqual(self.precalculation, other_precalculation)
+
+        with self.subTest("Test inequality with another number of epochs"):
+            other_precalculation = DistinctionPrecalculation(['no_shift', 'shift'], num_epochs=100)
             self.assertNotEqual(self.precalculation, other_precalculation)
 
         with self.subTest("Test inequality with another class"):
