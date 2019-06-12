34cc5a6174002e07985229ddd4375d014272e738
Justus Zeinert <zeinertj@amazon.com>
2019-06-12 15:17:10 +0200

    start adding visualization for numerical check

--- shift_detector/checks/statistical_checks/NumericalStatisticalCheck.py
+++ shift_detector/checks/statistical_checks/NumericalStatisticalCheck.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 from scipy import stats
 
 from shift_detector.checks.statistical_checks.StatisticalCheck import SimpleStatisticalCheck
@@ -20,3 +21,10 @@ class NumericalStatisticalCheck(SimpleStatisticalCheck):
 
     def statistical_test(self, part1: pd.Series, part2: pd.Series) -> float:
         return kolmogorov_smirnov_test(part1, part2)
+
+    def column_figure(self, column, df1, df2):
+        for data, color in [(df1[column], 'blue'), (df2[column], 'green')]:
+            mn, mx = min(pd.concat([df1[column], df2[column]])), max(pd.concat([df1[column], df2[column]])) + 1
+            bins = range(mn, mx, int((mx - mn) / 40))
+            plt.hist(data, bins=bins, color=color)
+            plt.show()
