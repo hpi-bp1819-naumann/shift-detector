094e4119a2222b5daa0a19efcc7768d98bfaf775
Mats Pörschke <ma-poerschke@t-online.de>
2019-06-14 09:59:05 +0200

    update fasttext vector size in to avoid Travis Memory Error

--- tests/checks/test_WordPredictionCheck.py
+++ tests/checks/test_WordPredictionCheck.py
@@ -20,7 +20,7 @@ class TestWordPredictionCheck(TestCase):
         self.df1 = DataFrame.from_dict(data1)
         self.df2 = DataFrame.from_dict(data2)
         self.store = Store(self.df1, self.df2)
-        self.check = WordPredictionCheck(relative_thresh=.3)
+        self.check = WordPredictionCheck(relative_thresh=.05, ft_size=50)
 
     def test_init(self):
         with self.subTest("Test wrong columns"):
