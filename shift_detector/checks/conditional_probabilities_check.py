9995ef0b64b55b5abb55884816732844fa941b1e
Leonard <l.v.merzljak@mailbox.org>
2019-06-14 10:08:15 +0200

    Add support for LDA embedding

--- shift_detector/checks/conditional_probabilities_check.py
+++ shift_detector/checks/conditional_probabilities_check.py
@@ -30,24 +30,33 @@ class ConditionalProbabilitiesCheck(Check):
         values exhibit an absolute difference of less than ``min_delta_supports`` are not printed.
     :param min_delta_confidences: a float between 0 and 1. Rules whose confidence
         values exhibit an absolute difference of less than ``min_delta_confidences`` are not printed.
+    :param number_of_bins: an int greater than 0. Numerical columns are binned into ``number_of_bins``
+        bins.
+    :param number_of_topics: an int greater than 0. Textual columns are embedded into ``number_of_topics``
+        topics.
     """
 
     def __init__(self, min_support=0.01, min_confidence=0.15, rule_limit=5, min_delta_supports=0.05,
-                 min_delta_confidences=0.05):
+                 min_delta_confidences=0.05, number_of_bins=50, number_of_topics=20):
         assert 0 <= min_support <= 1, 'min_support expects a float between 0 and 1'
         assert 0 <= min_confidence <= 1, 'min_confidence expects a float between 0 and 1'
         assert 0 <= rule_limit, 'rule_limit expects an int greater than or equal to 0'
         assert 0 <= min_delta_supports <= 1, 'min_delta_supports expects a float between 0 and 1'
         assert 0 <= min_delta_confidences <= 1, 'min_delta_confidences expects a float between 0 and 1'
+        assert 0 < number_of_bins, 'number_of_bins expects an int greater than 0'
+        assert 0 < number_of_topics, 'number_of_topics expects an int greater than 0'
         self.min_support = float(min_support)
         self.min_confidence = float(min_confidence)
         self.rule_limit = int(rule_limit)
         self.min_delta_supports = float(min_delta_supports)
         self.min_delta_confidences = float(min_delta_confidences)
+        self.number_of_bins = int(number_of_bins)
+        self.number_of_topics = int(number_of_topics)
 
     def run(self, store):
         rules, examined_columns = store[
-            ConditionalProbabilitiesPrecalculation(self.min_support, self.min_confidence)]
+            ConditionalProbabilitiesPrecalculation(self.min_support, self.min_confidence, self.number_of_bins,
+                                                   self.number_of_topics)]
 
         filtered_rules = (rule for rule in rules if abs(rule.delta_supports) >= self.min_delta_supports and abs(
             rule.delta_confidences) >= self.min_delta_confidences)
--- shift_detector/precalculations/conditional_probabilities_precalculation.py
+++ shift_detector/precalculations/conditional_probabilities_precalculation.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 from shift_detector.precalculations.binning_precalculation import BinningPrecalculation
+from shift_detector.precalculations.lda_embedding import LdaEmbedding
 from shift_detector.precalculations.conditional_probabilities import fpgrowth
 from shift_detector.precalculations.precalculation import Precalculation
 from shift_detector.utils.column_management import ColumnType
@@ -8,16 +9,19 @@ from shift_detector.utils.column_management import ColumnType
 
 class ConditionalProbabilitiesPrecalculation(Precalculation):
 
-    def __init__(self, min_support, min_confidence):
+    def __init__(self, min_support, min_confidence, number_of_bins, number_of_topics):
         self.min_support = min_support
         self.min_confidence = min_confidence
+        self.number_of_bins = number_of_bins
+        self.number_of_topics = number_of_topics
 
     def process(self, store):
         df1_cat, df2_cat = store[ColumnType.categorical]
-        df1_num, df2_num = store[BinningPrecalculation()]
+        df1_num, df2_num = store[BinningPrecalculation(self.number_of_bins)]
+        df1_text, df2_text = store[LdaEmbedding(self.number_of_topics)]
 
-        df1 = pd.concat([df1_cat, df1_num], axis=1)
-        df2 = pd.concat([df2_cat, df2_num], axis=1)
+        df1 = pd.concat([df1_cat, df1_num, df1_text], axis=1)
+        df2 = pd.concat([df2_cat, df2_num, df2_text], axis=1)
 
         rules = fpgrowth.calculate_frequent_rules(df1, df2, self.min_support, self.min_confidence)
         return rules, list(df1.columns)
@@ -25,7 +29,8 @@ class ConditionalProbabilitiesPrecalculation(Precalculation):
     def __eq__(self, other):
         if not isinstance(other, self.__class__):
             return False
-        return (self.min_support, self.min_confidence) == (other.min_support, other.min_confidence)
+        return (self.min_support, self.min_confidence, self.number_of_bins, self.number_of_topics) == (
+            other.min_support, other.min_confidence, other.number_of_bins, other.number_of_topics)
 
     def __hash__(self):
-        return hash((self.__class__, self.min_support, self.min_confidence))
+        return hash((self.__class__, self.min_support, self.min_confidence, self.number_of_bins, self.number_of_topics))
