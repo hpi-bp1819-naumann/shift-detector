00e2e497caafc36f25ad4b38b033eefbb505d42e
Finn <33052855+Mupico@users.noreply.github.com>
2019-06-17 09:58:25 +0200

    Refactor tests (#71)
    * pep8
    
    * all the tests are pep8
    
    * all files pep8 except for one where pep8 looks like sh*t
    
    * linebreak before operator
    
    * some testdata moved into test_data
    
    * setup in tests
    
    * fix merge
    
    * fix bug
    
    * style

--- tests/checks/statistical/test_numerical_statistical.py
+++ tests/checks/statistical/test_numerical_statistical.py
@@ -11,45 +11,76 @@ from shift_detector.precalculations.store import Store
 
 class TestCategoricalStatisticalCheck(unittest.TestCase):
 
+    def setUp(self):
+        self.not_significant_1 = ([2] * 1) +\
+                                 ([3] * 2) +\
+                                 ([4] * 0) +\
+                                 ([5] * 1) +\
+                                 ([6] * 2) +\
+                                 ([8] * 3) +\
+                                 ([9] * 2)
+
+        self.not_significant_2 = ([2] * 2) +\
+                                 ([3] * 1) +\
+                                 ([4] * 2) +\
+                                 ([5] * 2) +\
+                                 ([6] * 1) +\
+                                 ([8] * 1) +\
+                                 ([9] * 1)
+
+        self.kolmogorov_smirnov_1 = ([21] * 4) +\
+                                    ([23] * 11) +\
+                                    ([25] * 5) +\
+                                    ([27] * 7) +\
+                                    ([29] * 0) +\
+                                    ([31] * 5) +\
+                                    ([33] * 9) +\
+                                    ([35] * 13) +\
+                                    ([37] * 20) +\
+                                    ([39] * 6)
+
+        self.kolmogorov_smirnov_2 = ([21] * 7) +\
+                                    ([23] * 4) +\
+                                    ([25] * 1) +\
+                                    ([27] * 11) +\
+                                    ([29] * 12) +\
+                                    ([31] * 4) +\
+                                    ([33] * 2) +\
+                                    ([35] * 4) +\
+                                    ([37] * 8) +\
+                                    ([39] * 9)
+
+        self.significant_1 = ([21] * 7) +\
+                             ([23] * 4) +\
+                             ([25] * 1) +\
+                             ([27] * 11) +\
+                             ([29] * 12) +\
+                             ([31] * 4) +\
+                             ([33] * 2) +\
+                             ([35] * 4) +\
+                             ([37] * 2) +\
+                             ([39] * 9)
+
+        self.significant_2 = ([21] * 4) +\
+                             ([23] * 11) +\
+                             ([25] * 5) +\
+                             ([27] * 7) +\
+                             ([29] * 0) +\
+                             ([31] * 5) +\
+                             ([33] * 9) +\
+                             ([35] * 18) +\
+                             ([37] * 29) +\
+                             ([39] * 6)
+
     def test_kolmogorov_smirnov_test_result(self):
-        part1 = pd.Series(([21] * 4) +
-                          ([23] * 11) +
-                          ([25] * 5) +
-                          ([27] * 7) +
-                          ([29] * 0) +
-                          ([31] * 5) +
-                          ([33] * 9) +
-                          ([35] * 13) +
-                          ([37] * 20) +
-                          ([39] * 6))
-        part2 = pd.Series(([21] * 7) +
-                          ([23] * 4) +
-                          ([25] * 1) +
-                          ([27] * 11) +
-                          ([29] * 12) +
-                          ([31] * 4) +
-                          ([33] * 2) +
-                          ([35] * 4) +
-                          ([37] * 8) +
-                          ([39] * 9))
+        part1 = pd.Series(self.kolmogorov_smirnov_1)
+        part2 = pd.Series(self.kolmogorov_smirnov_2)
         p = kolmogorov_smirnov_test(part1, part2)
         self.assertAlmostEqual(0.043055, p, places=2)  # this should be equal in 5 places, but travis fails otherwise
 
     def test_not_significant(self):
-        df1 = pd.DataFrame(([2] * 1) +
-                           ([3] * 2) +
-                           ([4] * 0) +
-                           ([5] * 1) +
-                           ([6] * 2) +
-                           ([8] * 3) +
-                           ([9] * 2))
-        df2 = pd.DataFrame(([2] * 2) +
-                           ([3] * 1) +
-                           ([4] * 2) +
-                           ([5] * 2) +
-                           ([6] * 1) +
-                           ([8] * 1) +
-                           ([9] * 1))
+        df1 = pd.DataFrame(self.not_significant_1)
+        df2 = pd.DataFrame(self.not_significant_2)
         store = Store(df1, df2)
         result = NumericalStatisticalCheck().run(store)
         self.assertEqual(1, len(result.examined_columns))
@@ -57,26 +88,8 @@ class TestCategoricalStatisticalCheck(unittest.TestCase):
         self.assertEqual(0, len(result.explanation))
 
     def test_significant(self):
-        df1 = pd.DataFrame(([21] * 4) +
-                           ([23] * 11) +
-                           ([25] * 5) +
-                           ([27] * 7) +
-                           ([29] * 0) +
-                           ([31] * 5) +
-                           ([33] * 9) +
-                           ([35] * 18) +
-                           ([37] * 29) +
-                           ([39] * 6))
-        df2 = pd.DataFrame(([21] * 7) +
-                           ([23] * 4) +
-                           ([25] * 1) +
-                           ([27] * 11) +
-                           ([29] * 12) +
-                           ([31] * 4) +
-                           ([33] * 2) +
-                           ([35] * 4) +
-                           ([37] * 2) +
-                           ([39] * 9))
+        df1 = pd.DataFrame(self.significant_1)
+        df2 = pd.DataFrame(self.significant_2)
         store = Store(df1, df2)
         result = NumericalStatisticalCheck().run(store)
         self.assertEqual(1, len(result.examined_columns))
