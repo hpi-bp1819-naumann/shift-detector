0d39e09c52197117bfffe9277f37ef5cc75a612a
Malte Barth <malte.barth@student.hpi.de>
2019-06-17 10:48:35 +0200

    Merge branch 'lda_embedding' into lda

--- shift_detector/precalculations/lda_gensim_tokenizer.py
+++ shift_detector/precalculations/lda_gensim_tokenizer.py
@@ -9,37 +9,34 @@ import re
 class LdaGensimTokenizer(Precalculation):
 
     def __init__(self, cols, stop_words='english'):
-        self.stopwords = None
+        self.stopwords = set()
         self.cols = None
-        if isinstance(stop_words, str):
-            if stop_words in nltk_stopwords.fileids():
-                self.stop_words = nltk_stopwords.words(stop_words)
-            else:
-                raise ValueError('The language you entered is not available')
-        elif isinstance(stop_words, list) and all(isinstance(elem, str) for elem in stop_words):
-                for lang in stop_words:
-                    if lang not in nltk_stopwords.fileids():
-                        raise ValueError('At least one language you entered is not available')
-                    if self.stopwords is None:
-                        self.stopwords = set(nltk_stopwords.words(lang))
-                    else:
-                        self.stopwords = self.stopwords.union(set(nltk_stopwords.words(lang)))
-        else:
-            raise TypeError('Please enter the language for your stopwords as a string or list of strings')
 
-        if cols:
-            if isinstance(cols, list) and all(isinstance(col, str) for col in cols) or isinstance(cols, str):
-                self.cols = cols
-            else:
-                raise TypeError('Cols has to be list of strings or a single string')
+        if isinstance(stop_words, str):
+            if stop_words not in nltk_stopwords.fileids():
+                raise ValueError("The language you entered is not available. Received: {}".format(stop_words))
+            self.stop_words = nltk_stopwords.words(stop_words)
+        elif isinstance(stop_words, list) and all(isinstance(elem, str) for elem in stop_words):
+            for lang in stop_words:
+                if lang not in nltk_stopwords.fileids():
+                    raise ValueError("The following language you entered is not available: {}".format(lang))
+                self.stopwords = self.stopwords.union(set(nltk_stopwords.words(lang)))
         else:
-            raise TypeError('You have to specify which columns you want to tokenize')
+            raise TypeError("Please enter the language for your stop_words as a string or list of strings. Received: {}"
+                            .format(type(stop_words)))
+
+        if not cols:
+            raise TypeError("You have to specify which columns you want to tokenize")
+        if isinstance(cols, str):
+            self.cols = [cols]
+        elif isinstance(cols, list) and all(isinstance(col, str) for col in cols):
+            self.cols = cols
+        else:
+            raise TypeError("Cols has to be list of strings or a single string. Received: {}".format(type(cols)))
 
     def __eq__(self, other):
         """Overrides the default implementation"""
-        if isinstance(other, self.__class__) and self.stop_words == other.stop_words and self.cols == other.cols:
-            return True
-        return False
+        return isinstance(other, self.__class__) and self.stop_words == other.stop_words and self.cols == other.cols
 
     def __hash__(self):
         """Overrides the default implementation"""
@@ -51,40 +48,28 @@ class LdaGensimTokenizer(Precalculation):
     def process(self, store):
         df1_texts, df2_texts = store[ColumnType.text]
 
-        if isinstance(self.cols, str):
-            if self.cols in df1_texts.columns:
-                col_names = [self.cols]
-        else:
-            for col in self.cols:
-                if col not in df1_texts.columns:
-                    raise ValueError('Given column is not contained in given datasets')
-            col_names = self.cols
+        for col in self.cols:
+            if col not in store.column_names(ColumnType.text):
+                raise ValueError("Following given column is not contained in given datasets: {}".format(col))
+        col_names = self.cols
 
         processed1 = {}
         processed2 = {}
 
+        def make_usable_list(texts):
+            tokenized =[]
+            for entry in texts:
+                wordlist = []
+                for word in re.sub(r"[^\w+\s]|\b[a-zA-Z]\b", ' ', entry).split():
+                    if word == '' or word.lower() in self.stop_words:
+                        continue
+                    if len(gensim.utils.simple_preprocess(word)) > 0:
+                        wordlist.append(gensim.utils.simple_preprocess(word, deacc=True).pop())
+                tokenized.append(wordlist)
+            return tokenized
+
         for col in col_names:
-            tokenized1 = []
-            tokenized2 = []
-
-            for entry in df1_texts[col]:
-                wordlist = []
-                for word in re.sub(r"[^\w+\s]|\b[a-zA-Z]\b", ' ', entry).split():
-                    if word.lower() not in self.stop_words or '':
-                        if len(gensim.utils.simple_preprocess(word, deacc=True)) > 0:
-                            wordlist.append(gensim.utils.simple_preprocess(word, deacc=True).pop())
-                tokenized1.append(wordlist)
-
-            processed1[col] = tokenized1
-
-            for entry in df2_texts[col]:
-                wordlist = []
-                for word in re.sub(r"[^\w+\s]|\b[a-zA-Z]\b", ' ', entry).split():
-                    if word.lower() not in self.stop_words or '':
-                        if len(gensim.utils.simple_preprocess(word, deacc=True)) > 0:
-                            wordlist.append(gensim.utils.simple_preprocess(word, deacc=True).pop())
-                tokenized2.append(wordlist)
-
-            processed2[col] = tokenized2
+            processed1[col] = make_usable_list(df1_texts[col])
+            processed2[col] = make_usable_list(df2_texts[col])
 
         return pd.DataFrame.from_dict(processed1), pd.DataFrame.from_dict(processed2)
