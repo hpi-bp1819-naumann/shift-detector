60bd806456679f03385629e8347688e0ea701f35
Justus Zeinert <zeinertj@amazon.com>
2019-06-18 09:25:16 +0200

    remove absolute figures

--- shift_detector/checks/statistical_checks/numerical_statistical_check.py
+++ shift_detector/checks/statistical_checks/numerical_statistical_check.py
@@ -27,24 +27,6 @@ class NumericalStatisticalCheck(SimpleStatisticalCheck):
         return kolmogorov_smirnov_test(part1, part2)
 
     @staticmethod
-    def cumulative_hist_absolute_figure(column, df1, df2, bins=40):
-        _, bin_edges = np.histogram(pd.concat([df1[column], df2[column]]), bins=bins)
-        cumsum1, _, _ = plt.hist(df1[column], bins=bin_edges, align='right', color='cornflowerblue', cumulative=True,
-                                 histtype='step')
-        cumsum2, _, _ = plt.hist(df2[column], bins=bin_edges, align='right', alpha=0.5, color='seagreen',
-                                 cumulative=True, histtype='step')
-        distances = abs(cumsum1 - cumsum2)
-        max_idx = list(distances).index(max(distances))
-        max_d = max(distances)
-        plt.plot([bin_edges[max_idx], bin_edges[max_idx]], [cumsum1[max_idx], cumsum2[max_idx]],
-                 color='black', linewidth=1, linestyle='--')
-        plt.legend(['maximal distance = ' + str(max_d), column + ' 1', column + ' 2'], fontsize='x-small')
-        plt.title(column + ' (Cumulative Distribution)', fontsize='x-large')
-        plt.xlabel('column value', fontsize='medium')
-        plt.ylabel('number of rows', fontsize='medium')
-        plt.show()
-
-    @staticmethod
     def cumulative_hist_figure(column, df1, df2, bins=40):
         _, bin_edges = np.histogram(pd.concat([df1[column], df2[column]]), bins=bins)
         cumsum1, cumsum2 = plot_cumulative_step_ratio_histogram(df1[column], df2[column], bin_edges)
@@ -57,17 +39,6 @@ class NumericalStatisticalCheck(SimpleStatisticalCheck):
         plt.title(column + ' (Cumulative Distribution)', fontsize='x-large')
         plt.xlabel('column value', fontsize='medium')
         plt.ylabel('ratio of rows', fontsize='medium')
-        plt.show()
-
-    @staticmethod
-    def overlayed_hist_absolute_figure(column, df1, df2, bins=40):
-        _, bin_edges = np.histogram(pd.concat([df1[column], df2[column]]), bins=bins)
-        plt.hist(df1[column], bins=bin_edges, color='cornflowerblue')
-        plt.hist(df2[column], bins=bin_edges, alpha=0.5, color='seagreen')
-        plt.legend([column + ' 1', column + ' 2'], fontsize='x-small')
-        plt.title(column + ' (Histogram)', fontsize='x-large')
-        plt.xlabel('column value', fontsize='medium')
-        plt.ylabel('number of rows', fontsize='medium')
         plt.show()
 
     @staticmethod
