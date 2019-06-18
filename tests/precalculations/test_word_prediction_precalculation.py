fa6f3d94c7b68164348b53ef6e11367dc08f64f4
Mats Pörschke <ma-poerschke@t-online.de>
2019-06-18 14:16:36 +0200

    fix bugs

--- tests/precalculations/test_word_prediction_precalculation.py
+++ tests/precalculations/test_word_prediction_precalculation.py
@@ -1,6 +1,5 @@
 from unittest import TestCase
 
-import numpy as np
 from pandas import DataFrame
 
 from shift_detector.precalculations.store import Store
@@ -19,24 +18,34 @@ class TestWordPredictionPrecalculation(TestCase):
         for idx in range(len(alphabet) - 7):
             col.append(' '.join(alphabet[idx:idx+7]))
 
-        data1 = {'shift': col, 'no_shift': col}
-        data2 = {'shift': ['B B B B B B'] * len(col), 'no_shift': col}
+        error_col = []
+        for idx in range(len(col)):
+            error_col += [alphabet[idx]]
+
+        data1 = {'shift': col, 'no_shift': col, 'error_col': error_col}
+        data2 = {'shift': ['B B B B B B'] * len(col), 'no_shift': col, 'error_col': error_col}
         self.df1 = DataFrame.from_dict(data1)
         self.df2 = DataFrame.from_dict(data2)
         self.store = Store(self.df1, self.df2)
-        self.precalculation_shift = WordPredictionPrecalculation('shift', num_epochs_predictor=10)
-        self.precalculation_no_shift = WordPredictionPrecalculation('no_shift', num_epochs_predictor=10)
-
-        np.random.seed(1)
+        self.precalculation_shift = WordPredictionPrecalculation('shift', num_epochs_predictor=10,
+                                                                 ft_workers=2, ft_seed=0)
+        self.precalculation_no_shift = WordPredictionPrecalculation('no_shift', num_epochs_predictor=10,
+                                                                    ft_workers=1, ft_seed=0)
+        self.precalculation_error = WordPredictionPrecalculation('error_col', num_epochs_predictor=10,
+                                                                 ft_workers=1, ft_seed=0)
 
     def test_process(self):
         with self.subTest("Test losses"):
             df1_prediction_loss, df2_prediction_loss = self.precalculation_shift.process(self.store)
-            min_loss_diff = df1_prediction_loss * .3
+            min_loss_diff = df1_prediction_loss * .15
             self.assertTrue(df2_prediction_loss > df1_prediction_loss + min_loss_diff)
 
             df1_prediction_loss, df2_prediction_loss = self.precalculation_no_shift.process(self.store)
             self.assertTrue(df2_prediction_loss <= df1_prediction_loss)
+
+    def test_error_raising(self):
+        with self.subTest("Test error raising"):
+            self.assertRaises(ValueError, lambda: self.precalculation_error.process(self.store))
 
     def test_equal(self):
         with self.subTest("Test equality"):
