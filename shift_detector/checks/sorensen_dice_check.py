76cbac4f5b3666ce7fa914b9cf80a591e958dac9
Philipp Schmidt <scholl.8.st@googlemail.com>
2019-06-19 13:40:30 +0200

    SDC evaluation returns Dataframe

--- shift_detector/checks/sorensen_dice_check.py
+++ shift_detector/checks/sorensen_dice_check.py
@@ -1,8 +1,9 @@
-from collections import defaultdict
-
 from shift_detector.checks.check import Check, Report
 from shift_detector.precalculations.n_gram import NGramType
 from shift_detector.precalculations.sorensen_dice_precalculation import SorensenDicePrecalculations
+from IPython.display import display
+
+import pandas as pd
 
 
 class SorensenDiceCheck(Check):
@@ -16,18 +17,23 @@ class SorensenDiceCheck(Check):
 
         examined_columns = set(data.keys())
         shifted_columns = set()
-        explanation = defaultdict(str)
 
         for column_name in data:
             baseline1, baseline2, distance = data[column_name]
-            explanation[column_name] = "\tBaseline in Dataset1: {}\n" \
-                                       "\tBaseline in Dataset2: {}\n" \
-                                       "\tSorensen Dice Coefficient between Datasets: {}"\
-                .format(baseline1, baseline2, distance)
 
             if (abs(baseline1 - baseline2) > 0.1 or
                     distance - baseline1 < 0.1 or
                     distance - baseline2 < 0.1):
                 shifted_columns.add(column_name)
 
-        return Report("Sorensen Dice Check", examined_columns, shifted_columns, dict(explanation))
+        return SorensenDiceReport("Sorensen Dice Check", examined_columns, shifted_columns, information=data)
+
+
+class SorensenDiceReport(Report):
+
+    def print_information(self):
+        result_df = pd.DataFrame.from_dict(self.information, orient='index',
+                                           columns=['Baseline in Dataset 1',
+                                                    'Baseline in Dataset 2',
+                                                    'Distance between Datasets'])
+        display(result_df)
