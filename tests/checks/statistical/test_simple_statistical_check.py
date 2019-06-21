fba90146d042ea2ff361ff07f4cfb10fd566b11f
Justus Zeinert <zeinertj@amazon.com>
2019-06-21 14:30:20 +0200

    expand tests so coverage doesn't drop

--- tests/checks/statistical/test_simple_statistical_check.py
+++ tests/checks/statistical/test_simple_statistical_check.py
@@ -5,6 +5,7 @@ from matplotlib.figure import Figure
 from mock import MagicMock
 
 from shift_detector.checks.statistical_checks.categorical_statistical_check import CategoricalStatisticalCheck
+from shift_detector.checks.statistical_checks.numerical_statistical_check import NumericalStatisticalCheck
 
 
 class TestSimpleStatisticalCheck(unittest.TestCase):
@@ -14,18 +15,22 @@ class TestSimpleStatisticalCheck(unittest.TestCase):
         mock_figure = MagicMock(autospec=Figure)
         mock_plt_figure.return_value = mock_figure
         plot_functions = [MagicMock(), MagicMock(), MagicMock()]
-        CategoricalStatisticalCheck().plot_all_columns(plot_functions)  # using a subclass to instantiate
-        mock_plt_figure.assert_called_with(figsize=(10, 7.2), tight_layout=True)
-        for func in plot_functions:
-            self.assertTrue(func.called)
-        mock_figure.show.assert_called_with()
+        for check, height in [(CategoricalStatisticalCheck(), 7.2), (NumericalStatisticalCheck(), 10.8)]:
+            with self.subTest(check=check):
+                check.plot_all_columns(plot_functions)
+                mock_plt_figure.assert_called_with(figsize=(10, height), tight_layout=True)
+                for func in plot_functions:
+                    self.assertTrue(func.called)
+                mock_figure.show.assert_called_with()
 
     @mock.patch('shift_detector.checks.statistical_checks.statistical_check.plt.figure')
     @mock.patch('shift_detector.checks.statistical_checks.statistical_check.gridspec')
     def test_grid_is_created(self, mock_grid, mock_plt_figure):
         plot_functions = [MagicMock(), MagicMock(), MagicMock()]
-        CategoricalStatisticalCheck().plot_all_columns(plot_functions)  # using a subclass to instantiate
-        mock_grid.GridSpec.assert_called_with(2, 2)
+        for check, shape in [(CategoricalStatisticalCheck(), (2, 2)), (NumericalStatisticalCheck(), (3, 1))]:
+            with self.subTest(check=check):
+                check.plot_all_columns(plot_functions)
+                mock_grid.GridSpec.assert_called_with(shape[0], shape[1])
 
     def test_correct_number_of_plot_functions(self):
         for sig_cols in [['col1', 'col2'], ['col1'], []]:
