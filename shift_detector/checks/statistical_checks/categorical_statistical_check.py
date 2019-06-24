082fa06cdd8404d1a214fb7f7d2fa7f01b2a2d3f
Justus Zeinert <33062216+velramiir@users.noreply.github.com>
2019-06-24 16:46:55 +0200

    Combine statistical check plots (#124)
    combine the singular plots into a grid

--- shift_detector/checks/statistical_checks/categorical_statistical_check.py
+++ shift_detector/checks/statistical_checks/categorical_statistical_check.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+from matplotlib import gridspec
 from scipy import stats
 
 from shift_detector.checks.statistical_checks.statistical_check import SimpleStatisticalCheck
 from shift_detector.precalculations.low_cardinality_precalculation import LowCardinalityPrecalculation
+from shift_detector.utils import visualization as vis
 
 
 def chi2_test(part1: pd.Series, part2: pd.Series):
@@ -27,22 +29,20 @@ class CategoricalStatisticalCheck(SimpleStatisticalCheck):
         return chi2_test(part1, part2)
 
     @staticmethod
-    def paired_total_ratios_figure(column, df1, df2, top_k=50):
-        value_counts = pd.concat([df1[column].value_counts().head(top_k), df2[column].value_counts().head(top_k)],
-                                 axis=1).sort_index()
-        value_ratios = value_counts.fillna(0).apply(axis='columns',
-                                                    func=lambda row: pd.Series([row.iloc[0] / len(df1[column]),
-                                                                                row.iloc[1] / len(df2[column])],
-                                                                               index=[str(column) + ' 1',
-                                                                                      str(column) + ' 2']))
-        axes = value_ratios.plot(kind='barh', fontsize='medium')
+    def paired_total_ratios_plot(figure, axes, column, df1, df2, top_k=15):
+        axes = vis.plot_categorical_horizontal_ratio_histogram(axes, (df1[column], df2[column]), top_k)
         axes.invert_yaxis()  # to match order of legend
-        column_name = column if isinstance(column, str) else '_'.join(column)
-        axes.set_title('Column: ' + column_name, fontsize='x-large')
+        column_name = str(column)
+        axes.set_title('Column: {}'.format(column_name), fontsize='x-large')
         axes.set_xlabel('value ratio', fontsize='medium')
         axes.set_ylabel('column value', fontsize='medium')
-        plt.show()
+        figure.add_subplot(axes)
+
+    def number_of_columns_of_plots(self) -> int:
+        return 2
 
     @staticmethod
-    def column_figure(column, df1, df2):
-        CategoricalStatisticalCheck.paired_total_ratios_figure(column, df1, df2)
+    def column_plot(figure, tile, column, df1, df2):
+        CategoricalStatisticalCheck.paired_total_ratios_plot(figure=figure,
+                                                             axes=plt.Subplot(figure, tile),
+                                                             column=column, df1=df1, df2=df2)
---
+++ tests/checks/statistical/test_simple_statistical_check.py
@@ -0,0 +1,37 @@
+import unittest
+from unittest import mock
+
+from matplotlib.figure import Figure
+from mock import MagicMock
+
+from shift_detector.checks.statistical_checks.categorical_statistical_check import CategoricalStatisticalCheck
+from shift_detector.checks.statistical_checks.numerical_statistical_check import NumericalStatisticalCheck
+
+
+class TestSimpleStatisticalCheck(unittest.TestCase):
+
+    @mock.patch('shift_detector.checks.statistical_checks.statistical_check.plt')
+    def test_all_plot_functions_are_called_and_plot_is_shown(self, mock_plt):
+        plot_functions = [MagicMock(), MagicMock(), MagicMock()]
+        for check, height in [(CategoricalStatisticalCheck(), 10), (NumericalStatisticalCheck(), 15.0)]:
+            with self.subTest(check=check):
+                check.plot_all_columns(plot_functions)
+                mock_plt.figure.assert_called_with(figsize=(12, height), tight_layout=True)
+                for func in plot_functions:
+                    self.assertTrue(func.called)
+                mock_plt.show.assert_called_with()
+
+    @mock.patch('shift_detector.checks.statistical_checks.statistical_check.plt.figure')
+    @mock.patch('shift_detector.checks.statistical_checks.statistical_check.gridspec')
+    def test_grid_is_created(self, mock_grid, mock_plt_figure):
+        plot_functions = [MagicMock(), MagicMock(), MagicMock()]
+        for check, shape in [(CategoricalStatisticalCheck(), (2, 2)), (NumericalStatisticalCheck(), (3, 1))]:
+            with self.subTest(check=check):
+                check.plot_all_columns(plot_functions)
+                mock_grid.GridSpec.assert_called_with(shape[0], shape[1])
+
+    def test_correct_number_of_plot_functions(self):
+        for sig_cols in [['col1', 'col2'], ['col1'], []]:
+            with self.subTest(sig_cols=sig_cols):
+                result = CategoricalStatisticalCheck().plot_functions(sig_cols, MagicMock(), MagicMock())
+        self.assertEqual(len(sig_cols), len(result))
