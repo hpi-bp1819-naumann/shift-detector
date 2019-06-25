145ecf1820eff575453be0f12c22b6c45b6db50d
Philipp Schmidt <philipp.schmidt@student.hpi.de>
2019-06-25 14:43:41 +0200

    threshold and nicer output for sorensen dice and embedding distance check

--- shift_detector/checks/embedding_distance_check.py
+++ shift_detector/checks/embedding_distance_check.py
@@ -1,4 +1,3 @@
-from collections import defaultdict
 from shift_detector.checks.check import Check, Report
 from shift_detector.precalculations.embedding_distance_precalculation import EmbeddingDistancePrecalculation
 from IPython.display import display
@@ -8,32 +7,35 @@ import pandas as pd
 
 class EmbeddingDistanceCheck(Check):
 
-    def __init__(self, model='word2vec', trained_model=None):
+    def __init__(self, model='word2vec', trained_model=None, threshold=3.0):
         self.model = model
         self.trained_model = trained_model
+        self.threshold = threshold
 
     def run(self, store):
         data = store[EmbeddingDistancePrecalculation(model=self.model, trained_model=self.trained_model)]
 
         examined_columns = set(data.keys())
         shifted_columns = set()
-        explanation = defaultdict(str)
 
         for column_name in data:
             baseline1, baseline2, distance = data[column_name]
 
-            if (abs(baseline1 - baseline2) > baseline1 * 3
-                    or abs(baseline1 - baseline2) > baseline2 * 3
-                    or abs(baseline1 - distance) > baseline1 * 3
-                    or abs(baseline2 - distance) > baseline2 * 3):
+            if (abs(baseline1 - baseline2) > baseline1 * self.threshold
+                    or abs(baseline1 - baseline2) > baseline2 * self.threshold
+                    or abs(baseline1 - distance) > baseline1 * self.threshold
+                    or abs(baseline2 - distance) > baseline2 * self.threshold):
                 shifted_columns.add(column_name)
 
-        return EmbeddingDistanceReport("Embedding Distance Check", examined_columns, shifted_columns, dict(explanation), information=data)
+        return EmbeddingDistanceReport("Embedding Distance Check", examined_columns, shifted_columns,
+                                       information=(self.threshold, data))
 
 
 class EmbeddingDistanceReport(Report):
 
     def print_information(self):
-        result_df = pd.DataFrame.from_dict(self.information, orient='index')
-        result_df.columns = ['Baseline in Dataset 1', 'Baseline in Dataset 2', 'Distance between Datasets']
-        display(result_df)
+        result_df = pd.DataFrame.from_dict(self.information[1], orient='index')
+        result_df.columns = ['Distance within Dataset 1', 'Distance within Dataset 2',
+                             'Distance between Datasets']
+        result_df['Rel. Threshold'] = self.information[0]
+        display(result_df.loc[self.shifted_columns])
