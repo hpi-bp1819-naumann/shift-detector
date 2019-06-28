21476f29840ca1407db04838eabefcce8ac036fb
Malte Barth <malte.barth@student.hpi.de>
2019-06-28 13:32:48 +0200

    test gensim for lda check

--- tests/checks/test_lda_check.py
+++ tests/checks/test_lda_check.py
@@ -7,10 +7,10 @@ import pandas as pd
 class TestLdaCheck(unittest.TestCase):
 
     def setUp(self):
-        self.lda_report1 = LdaCheck(significance=0.1, n_topics=2, n_iter=1, random_state=0)
-        self.lda_report2 = LdaCheck(significance=0.1, n_topics=2, n_iter=1, random_state=0)
-        self.lda_report3 = LdaCheck(cols=['text'], significance=0.1, n_topics=2, n_iter=1, random_state=0)
-        self.lda_report4 = LdaCheck(cols=['abcd'], significance=0.1, n_topics=2, n_iter=1, random_state=0)
+        self.lda_report1 = LdaCheck(lib='sklearn', significance=0.1, n_topics=2, n_iter=1, random_state=0)
+        self.lda_report2 = LdaCheck(lib='sklearn', significance=0.1, n_topics=2, n_iter=1, random_state=0)
+        self.lda_report3 = LdaCheck(lib='gensim', cols=['text'], significance=0.1, n_topics=2, n_iter=8, random_state=0)
+        self.lda_report4 = LdaCheck(lib='gensim', cols=['abcd'], significance=0.1, n_topics=2, n_iter=8, random_state=0)
 
         self.poems = [
             'Tell me not, in mournful numbers,\nLife is but an empty dream!\nFor the soul is dead that slumbers,\nAnd things are not what they seem.',
@@ -166,24 +166,24 @@ class TestLdaCheck(unittest.TestCase):
         self.df2 = pd.DataFrame(self.phrases, columns=['text'])
         self.store = Store(self.df1, self.df2)
 
-    def test_exception_on_small_n(self):
+    def test_exception_on_significance(self):
         self.assertRaises(ValueError, lambda: LdaCheck(significance=0.0))
-
-    def test_exception_on_non_float(self):
         self.assertRaises(TypeError, lambda: LdaCheck(significance=42))
         self.assertRaises(TypeError, lambda: LdaCheck(significance='abcd'))
 
+    def test_exception_on_cols(self):
+        self.assertRaises(TypeError, lambda: LdaCheck(cols=42))
+
     def test_run(self):
-        with self.subTest("Test successful run without specifying the 'cols' parameter"):
-            report = self.lda_report1.run(self.store)
-            self.assertAlmostEqual(report.explanation['Topic 1 diff in column text'], 0.5)
-            self.assertAlmostEqual(report.explanation['Topic 2 diff in column text'], -0.5)
+        with self.subTest("Test successful run without specifying the 'cols' parameter with sklearn"):
+            report1 = self.lda_report1.run(self.store)
+            self.assertAlmostEqual(report1.explanation['Topic 1 diff in column text'], 0.5)
+            self.assertAlmostEqual(report1.explanation['Topic 2 diff in column text'], -0.5)
 
-        with self.subTest("Test successful run with specifying the 'cols' parameter"):
-            report = self.lda_report3.run(self.store)
-
-            self.assertAlmostEqual(report.explanation['Topic 1 diff in column text'], 0.5)
-            self.assertAlmostEqual(report.explanation['Topic 2 diff in column text'], -0.5)
+        with self.subTest("Test successful run with specifying the 'cols' parameter with gensim"):
+            report2 = self.lda_report3.run(self.store)
+            self.assertAlmostEqual(report2.explanation['Topic 1 diff in column text'], -0.122)
+            self.assertAlmostEqual(report2.explanation['Topic 2 diff in column text'], 0.122)
 
         with self.subTest("Test unsuccessful run with specifying a wrong 'cols' parameter"):
             self.assertRaises(ValueError, lambda: self.lda_report4.run(self.store))
